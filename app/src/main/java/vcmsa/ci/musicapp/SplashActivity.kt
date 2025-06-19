4b416d098add6f15119f2cde2fc1e78123f9c4ef
Likhona Ngwenya <ST10446272@vcconnect.edu.za>
2025-06-19 09:54:15 +0200

    UI Elements

---
+++ app/src/main/java/vcmsa/ci/musicapp/SplashActivity.kt
@@ -0,0 +1,16 @@
+package vcmsa.ci.musicapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+
+
+    }
+}
